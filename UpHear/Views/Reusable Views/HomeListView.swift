d997bc85a55ee11160120e2af5c0eeae9f9ae4b6
tim-nnd <timothy.ananda@gmail.com>
2021-07-29 13:49:49 +0700

    Add reusable views group

---
+++ UpHear/Views/Reusable Views/HomeListView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeListView.swift
+//  UpHear
+//
+//  Created by Timothy Ananda on 29/07/21.
+//
+
+import SwiftUI
+
+struct HomeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeListView()
+    }
+}
