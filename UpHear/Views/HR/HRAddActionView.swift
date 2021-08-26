a88b862d6522dd98bbf0f74f6b745c30663a44c3
Bismo Asyura Widianto <bismo.a.widianto@gmail.com>
2021-08-26 23:06:11 +0700

    Tambah custom DatePicker di HRAddAction

--- UpHear/Views/HR/HRAddActionView.swift
+++ UpHear/Views/HR/HRAddActionView.swift
@@ -17,11 +17,14 @@ struct HRAddActionView: View {
         UINavigationBar.appearance().shadowImage = UIImage()
     }
     
+    
     @State private var actionTitle: String = ""
     @State private var details: String = "Describe the action"
     @State private var isRequiredResponse: Bool = true
     @State private var isRequiredSubmitFile: Bool = true
-    @State private var date: String = ""
+    @State private var date = Date()
+    
+    @State var showDatePicker = false
     
     var body: some View {
         NavigationView{
@@ -34,117 +37,129 @@ struct HRAddActionView: View {
                             .frame(width: 390, height: 40)
                         Spacer()
                         
-                        VStack{
-                            VStack(alignment: .leading){
-                                Text("Action Title")
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.primaryColor)
-                                    .font(Font.system(size: 16))
-                                
-                                TextField("Enter title of action", text: $actionTitle)
-                                    .padding(.leading,16)
-                                    .frame(width: 342, height: 44, alignment: .center)
-                                    .border(Colors.primaryColor, width: 2)
-                                    .cornerRadius(4)
-                            }.padding(.top,16)
+                        ZStack{
                             
-                            VStack(alignment: .leading){
-                                Text("Details")
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.primaryColor)
-                                    .font(Font.system(size: 16))
-                                    .padding(.bottom, -2)
-                                
-                                //                            TextField("Enter title of action", text: $details)
-                                //                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                                //                                .frame(width: 342, height: 44, alignment: .center)
-                                
-                                TextField(
-                                    " ",
-                                    text: $details
-                                    
-                                ).padding(.leading,16)
-                                .frame(width: 342, height: 171, alignment: .leading)
-                                .border(Color.primaryColor, width: 2)
-                                .cornerRadius(4)
-                            }.padding(.top,16)
-                            
-                            VStack(alignment: .leading){
-                                Text("Action Estimation Deadline")
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.primaryColor)
-                                    .font(Font.system(size: 16))
-                                
-                                
-                                TextField("Enter Date", text: $date)
-                                    .padding(.leading,16)
-                                    .frame(width: 342, height: 44, alignment: .center)
-                                    .border(Colors.primaryColor, width: 2)
-                                    .cornerRadius(4)
-                            }.padding(.top,16)
-                            
-                            
-                            HStack{
-                                Text("Required response")
-                                    .padding(.leading,16)
-                                Spacer()
-                                Toggle("Sound", isOn: $isRequiredResponse)
-                                    .labelsHidden()
-                                    .padding()
-                            }.frame(width: 342, height: 44, alignment: .center)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 4)
-                                    .stroke(lineWidth: 2)
-                                    .foregroundColor(.primaryColor)
-                            ).padding(.top,40)
-                            
-                            
-                            HStack{
-                                Text("Required submit file")
-                                    .padding()
-                                Spacer()
-                                Toggle("Sound", isOn: $isRequiredSubmitFile)
-                                    .labelsHidden()
-                                    .padding()
+                            VStack{
+                                DatePicker("", selection: $date, displayedComponents: [.date]).datePickerStyle(GraphicalDatePickerStyle())
+                                Button(action: {
+                                    showDatePicker = false
+                                }, label: {
+                                    Text("Done")
+                                })
                             }
-                            .frame(width: 342, height: 44, alignment: .center)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 4)
-                                    .stroke(lineWidth: 2)
-                                    .foregroundColor(.primaryColor)
-                            )
-                            .padding(.top,16)
-                            //                    .padding(.bottom,102)
+                            .padding(40)
+                            .opacity(showDatePicker ? 1 : 0)
                             
+                            VStack{
+                                VStack(alignment: .leading){
+                                    Text("Action Title")
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(.primaryColor)
+                                        .font(Font.system(size: 16))
+                                    
+                                    TextField("Enter title of action", text: $actionTitle)
+                                        .padding(.leading,16)
+                                        .frame(width: 342, height: 44, alignment: .center)
+                                        .border(Colors.primaryColor, width: 2)
+                                        .cornerRadius(4)
+                                }.padding(.top,16)
+                                
+                                VStack(alignment: .leading){
+                                    Text("Details")
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(.primaryColor)
+                                        .font(Font.system(size: 16))
+                                        .padding(.bottom, -2)
+                                    
+                                    TextField(
+                                        " ",
+                                        text: $details
+                                        
+                                    ).padding(.leading,16)
+                                    .frame(width: 342, height: 171, alignment: .leading)
+                                    .border(Color.primaryColor, width: 2)
+                                    .cornerRadius(4)
+                                }.padding(.top,16)
+                                
+                                VStack(alignment: .leading){
+                                    Text("Action Estimation Deadline")
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(.primaryColor)
+                                        .font(Font.system(size: 16))
+                                    
+                                    Button(action: {
+                                        showDatePicker = true
+                                    }, label: {
+                                        HStack{
+                                            Text("\(date)")
+                                                .foregroundColor(Colors.customGray)
+                                                .multilineTextAlignment(.leading)
+                                                .padding()
+                                            Spacer()
+                                        }
+                                    }).frame(maxWidth: 342, maxHeight: 45)
+                                    .background(Color.white)
+                                    .border(Color.primaryColor, width: 2)
+                                    .cornerRadius(4)
+                                    Spacer().frame(height: 24)
+                                    
+                                    
+                                }.padding(.top,16)
+                                
+                                
+                                HStack{
+                                    Text("Required response")
+                                        .padding(.leading,16)
+                                    Spacer()
+                                    Toggle("Sound", isOn: $isRequiredResponse)
+                                        .labelsHidden()
+                                        .padding()
+                                }.frame(width: 342, height: 44, alignment: .center)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 4)
+                                        .stroke(lineWidth: 2)
+                                        .foregroundColor(.primaryColor)
+                                ).padding(.top,40)
+                                
+                                
+                                HStack{
+                                    Text("Required submit file")
+                                        .padding()
+                                    Spacer()
+                                    Toggle("Sound", isOn: $isRequiredSubmitFile)
+                                        .labelsHidden()
+                                        .padding()
+                                }
+                                .frame(width: 342, height: 44, alignment: .center)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 4)
+                                        .stroke(lineWidth: 2)
+                                        .foregroundColor(.primaryColor)
+                                )
+                                .padding(.top,16)
+                                
+                                Button(action: { }) {
+                                    Rectangle().fill(Colors.primaryColor)
+                                        .cornerRadius(8)
+                                        .frame(width: 342, height:50)
+                                        .padding(.horizontal, 31).overlay(
+                                            Text("Add Action")
+                                                .accentColor(.white)
+                                        )
+                                }.padding(.top,60)
+                                Spacer()
+                            }.opacity(showDatePicker ? 0 : 1)
                             
-                            Button(action: { }) {
-                                Rectangle().fill(Colors.primaryColor)
-                                    .cornerRadius(8)
-                                    .frame(width: 342, height:50)
-                                    .padding(.horizontal, 31).overlay(
-                                        Text("Add Action")
-                                            .accentColor(.white)
-                                    )
-                            }.padding(.top,60)
-                            Spacer()
                         }
-                        
+                        .navigationTitle("TITLE")
+                        .navigationBarTitleDisplayMode(.inline)
+                        .navigationBarItems(leading: Button(action: { }) {
+                            Image(systemName: "chevron.left").foregroundColor(.white).font(.system(size: 24).weight(.semibold))
+                        })
                     }
-                    
-                    
-                    .navigationTitle("TITLE")
-                    .navigationBarTitleDisplayMode(.inline)
-                    .navigationBarItems(leading: Button(action: { }) {
-                        Image(systemName: "chevron.left").foregroundColor(.white).font(.system(size: 24).weight(.semibold))
-                    })
-                    
                 }
             }
-            
-            
         }
-        
-        
     }
 }
 
