c9416886203c37cee077cdf7edc6d00044464364
OidneR <michaelrandio2000@gmail.com>
2021-08-29 21:49:37 +0700

    added action sheet

--- UpHear/ViewModels/CreateReportViewModel.swift
+++ UpHear/ViewModels/CreateReportViewModel.swift
@@ -11,6 +11,7 @@ import FirebaseStorage
 
 class CreateReportViewModel: ObservableObject {
     @Published var caseReport: Case = Case()
+    @Published var willDismiss: Bool = false
     @Published var currentIndex: Int = 1
     @Published var incidentDate: Date = Date()
     @Published var IncidentPlace: String = ""
@@ -66,7 +67,7 @@ class CreateReportViewModel: ObservableObject {
                     }
                     self.arrayOfEvidence.append(url.absoluteString)
                     print(url.absoluteString)
-                    if(index == self.arrayOfEvidenceImage.count-1){
+                    if(self.arrayOfEvidence.count == self.arrayOfEvidenceImage.count){
                         // upload to airtable
                     }
                 }
@@ -75,32 +76,3 @@ class CreateReportViewModel: ObservableObject {
     }
 }
 
-extension View {
-// This function changes our View to UIView, then calls another function
-// to convert the newly-made UIView to a UIImage.
-    public func asUIImage() -> UIImage {
-        let controller = UIHostingController(rootView: self)
-        
-        controller.view.frame = CGRect(x: 0, y: CGFloat(Int.max), width: 1, height: 1)
-        UIApplication.shared.windows.first!.rootViewController?.view.addSubview(controller.view)
-        
-        let size = controller.sizeThatFits(in: UIScreen.main.bounds.size)
-        controller.view.bounds = CGRect(origin: .zero, size: size)
-        controller.view.sizeToFit()
-        
-// here is the call to the function that converts UIView to UIImage: `.asUIImage()`
-        let image = controller.view.asUIImage()
-        controller.view.removeFromSuperview()
-        return image
-    }
-}
-
-extension UIView {
-// This is the function to convert UIView to UIImage
-    public func asUIImage() -> UIImage {
-        let renderer = UIGraphicsImageRenderer(bounds: bounds)
-        return renderer.image { rendererContext in
-            layer.render(in: rendererContext.cgContext)
-        }
-    }
-}
